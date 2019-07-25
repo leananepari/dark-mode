8828fee67984e1acb8c05011bfd889d9132cc003
Leana <leananeparidze.com>
2019-07-25 12:47:07 -0700

    adds useLocalStorage file

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValues, setStoredValues] = useState(() => {
+    const item = window.localStorage.getItem(key);
+
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  function setValue(value) {
+    setStoredValues(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  return [storedValues, setValue];
+}
